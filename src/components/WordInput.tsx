fb8b6a73fd0aafd76729ac40268f41884302bd24
Viliam <viliamboris06@gmail.com>
2025-06-16 03:29:30 +0200

    zmena farby input fieldov (lepsia viditelnost)

--- src/components/WordInput.tsx
+++ src/components/WordInput.tsx
@@ -50,8 +50,8 @@ export default function WordInput({ value, onChange, onSubmit, disabled, letterS
             disabled
               ? 'bg-gray-100 border-gray-300 text-gray-500'
               : isValid
-              ? 'border-green-500 focus:border-green-600'
-              : 'border-gray-300 focus:border-purple-500'
+              ? 'border-green-500 focus:border-green-600 text-gray-900'
+              : 'border-gray-300 focus:border-purple-500 text-gray-900'
           }`}
           placeholder={`Word with "${letterSequence}"`}
           maxLength={30}
